8f18e7fb73f5dcc07178a2dde91c9129a023ef47
Lassehoutenbos <66307118+Lassehoutenbos@users.noreply.github.com>
2024-05-21 17:45:37 +0200

    Console fix

--- Code/CNC_Interpreter_V2/GPIOControl.cs
+++ Code/CNC_Interpreter_V2/GPIOControl.cs
@@ -146,7 +146,7 @@ namespace CNC_Interpreter_V2
             double DutyCycle = (double)Speed / 100;
             _setPWM(state, 1, 0, DutyCycle);
             _setPin(6, Dir);
-            Console.WriteLine("Dutycycle:" ,DutyCycle);
+            Console.WriteLine(DutyCycle);
             return true;
 
         }
