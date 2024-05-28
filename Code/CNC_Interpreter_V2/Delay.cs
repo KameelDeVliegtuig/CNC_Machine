dd755c2499264e8300f2f1f188ad92c2a5830cd2
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-05-28 17:07:44 +0200

    Removed UsDelay from GPIOControl to non blocking class

---
+++ Code/CNC_Interpreter_V2/Delay.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CNC_Interpreter_V2
+{
+	internal class Delay : IDisposable
+	{
+
+		public event EventHandler DelayComplete;
+
+		// Delay for microseconds
+		public async Task UsDelay(int microseconds, long StartTick)
+		{
+			var targetTicks = ((microseconds) * Stopwatch.Frequency) / 1000000;
+			while (Stopwatch.GetTimestamp() - StartTick < targetTicks)
+			{
+				await Task.Yield(); // Non-blocking wait
+			}
+
+			OnDelayComplete(EventArgs.Empty);
+		}
+
+		protected virtual void OnDelayComplete(EventArgs e)
+		{
+			DelayComplete?.Invoke(this, e);
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				// Dispose managed resources
+				DelayComplete = null;
+			}
+		}
+	}
+}
