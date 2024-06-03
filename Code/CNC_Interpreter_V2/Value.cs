32a364973e3893fb46ce8560a324ee39771099bc
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-06-03 17:36:58 +0200

    More warnings removed

--- Code/CNC_Interpreter_V2/Value.cs
+++ Code/CNC_Interpreter_V2/Value.cs
@@ -34,7 +34,7 @@ namespace CNC_Interpreter_V2
         public double R { get { return r; } set { if (!Double.IsNaN(value)) { r = value; }; } }
         public double S { get { return s; } set { if (value.GetType() == typeof(double)) { s = value; }; } }
         public Settings.Workplanes Workplane { get { return workplane; } set { if (value.GetType() == typeof(Settings.Workplanes)) { workplane = value; } } }
-        public bool MM { get { return MM; } set { MM = value; } }
+        public bool MM { get { return MM; } set { mm = value; } }
 
         public void Print()
         {
