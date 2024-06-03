41aba7b69617a316902cf94b1a1eb94273ccebf4
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-06-03 12:42:34 +0200

    minus value

--- Code/CNC_Interpreter_V2/Interpreter.cs
+++ Code/CNC_Interpreter_V2/Interpreter.cs
@@ -574,13 +574,13 @@ namespace CNC_Interpreter_V2
             if (Axis == GPIOControl.StepperAxis.X)
             {
                 Up = new Coordinate(backDistance, 0, 0, false);
-                Down = new Coordinate(-0.1, 0, 0, false);
+                Down = new Coordinate((-0.1), 0, 0, false);
             }
 
             if (Axis == GPIOControl.StepperAxis.Y)
             {
                 Up = new Coordinate(0, backDistance, 0, false);
-                Down = new Coordinate(0, -0.1, 0, false);
+                Down = new Coordinate(0, (-0.1), 0, false);
             }
 
             if (Axis == GPIOControl.StepperAxis.Z)
@@ -591,7 +591,7 @@ namespace CNC_Interpreter_V2
                 while (!axisControl.ReadLimitSwitch(GPIOControl.LimitSwitch.Z)) continue;
                 Thread.Sleep(2000);
                 Up = new Coordinate(0, 0, backDistance, false);
-                Down = new Coordinate(0, 0, -0.1, false);
+                Down = new Coordinate(0, 0, (-0.1), false);
             }
 
             Console.WriteLine("Up: ");
