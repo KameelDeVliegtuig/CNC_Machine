06c6e341b11086c198135441df7a73cd1aa62746
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-06-03 13:05:39 +0200

    less cluttering

--- Code/CNC_Interpreter_V2/Interpreter.cs
+++ Code/CNC_Interpreter_V2/Interpreter.cs
@@ -594,12 +594,6 @@ namespace CNC_Interpreter_V2
                 Down = new Coordinate(0, 0, (-0.1), false);
             }
 
-            Console.WriteLine("Up: ");
-            Console.WriteLine("X: " + Up.X + ", Y: " + Up.Y + ", Z: ", Up.Z);
-
-            Console.WriteLine("Down: ");
-            Console.WriteLine("X: " + Down.X + ", Y: " + Down.Y + ", Z: ", Down.Z);
-
 
             Console.WriteLine("Starting Auto Home");
             // Move axis until it is touching limitswitch
