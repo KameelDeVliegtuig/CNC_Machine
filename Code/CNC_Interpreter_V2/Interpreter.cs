6867141e5b8a851c280cbb644aeb042d1f85d38a
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-06-03 12:38:54 +0200

    little bit more of delay just because

--- Code/CNC_Interpreter_V2/Interpreter.cs
+++ Code/CNC_Interpreter_V2/Interpreter.cs
@@ -589,7 +589,7 @@ namespace CNC_Interpreter_V2
                 Console.WriteLine("Connect Z-axis limit switch and press manually");
                 while (axisControl.ReadLimitSwitch(GPIOControl.LimitSwitch.Z)) continue;
                 while (!axisControl.ReadLimitSwitch(GPIOControl.LimitSwitch.Z)) continue;
-                Thread.Sleep(500);
+                Thread.Sleep(2000);
                 Up = new Coordinate(0, 0, backDistance, false);
                 Down = new Coordinate(0, 0, -0.1, false);
             }
