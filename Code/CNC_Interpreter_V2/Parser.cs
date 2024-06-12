f447b3b82b47af68293867cc0bfabbc0f69e00af
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-06-12 14:27:06 +0200

    fixed G0 and G1 no coordinates

--- Code/CNC_Interpreter_V2/Parser.cs
+++ Code/CNC_Interpreter_V2/Parser.cs
@@ -10,17 +10,25 @@ namespace CNC_Interpreter_V2
     internal class Parser
     {
         private Calculator _calculator = new Calculator(0.005);
+        private string lastCommand = string.Empty;
         public List<Coordinate> Parse(double[] current, Value Input)
         {
             List<Coordinate> result = new List<Coordinate>();
             switch (Input.Command)
             {
                 case "G0": // Linear move, spindel off
-                    Coordinate G0 = new Coordinate(Input.X, Input.Y, Input.Z, false);
-                    result.Add(G0);
+                    lastCommand = Input.Command;
+                    if (Input.X != -0.0 || Input.Y != -0.0 || Input.Z != -0.0)
+                    {
+                        result.Add(new Coordinate(Input.X, Input.Y, Input.Z, false));
+                    }
                     break;
                 case "G1": // Linear move, spindel on
-                    result.Add(new Coordinate(Input.X, Input.Y, Input.Z, true));
+                    lastCommand = Input.Command;
+                    if (Input.X != -0.0 || Input.Y != -0.0 || Input.Z != -0.0)
+                    {
+                        result.Add(new Coordinate(Input.X, Input.Y, Input.Z, true));
+                    }
                     break;
                 case "G2": // Clockwise arc
                 case "G3": // CounterClockwise arc
@@ -42,7 +50,7 @@ namespace CNC_Interpreter_V2
                     {
                         offset = new[] { Input.I, Input.J };
                     }
-                    if(Input.R != -0.0)
+                    if (Input.R != -0.0)
                     {
                         radius = Input.R;
                     }
