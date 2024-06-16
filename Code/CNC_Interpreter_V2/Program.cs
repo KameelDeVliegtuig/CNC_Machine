2b3a6e7b9594e57be6c7ec2b927c75ff2b67150d
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-06-16 17:19:43 +0200

    Added additional thread for executing moves

--- Code/CNC_Interpreter_V2/Program.cs
+++ Code/CNC_Interpreter_V2/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CNC_Interpreter_V2;
+using Iot.Device.Display;
 using Iot.Device.Nmea0183.Ais;
 using System.ComponentModel.Design;
 using System.Device.Pwm;
@@ -17,6 +18,7 @@ PresenceDetector presenceDetector = new PresenceDetector("/dev/ttyUSB0", 115200)
 Coordinate coordinate = new Coordinate(10, 10, 10, true);
 AxisControl axisControl = new AxisControl(25, null);
 
+Thread GCodeThread = new(RunGCodes);
 
 
 //if (args.Length == 0)
@@ -32,10 +34,10 @@ AxisControl axisControl = new AxisControl(25, null);
 //}
 
 Console.WriteLine("Program started!");
+GCodeThread.Start();
 
 while (true)
 {
-
     string? GCode = Console.ReadLine();
     if (GCode == null) break;
     try
@@ -46,18 +48,15 @@ while (true)
     {
         Console.WriteLine(e);
     }
-    Console.WriteLine("Moves length: " + interpreter.Moves.Count());
-    for (int i = 0; i < interpreter.Moves.Count; i++)
-    {
-        if (Globals.brake || Globals.stop)
-        {
-            break;
-        }
-        interpreter.Moves[0].Print();
-        axisControl.Move(interpreter.Moves[0]);
-        interpreter.Moves.RemoveAt(0);
-    }
+    Console.WriteLine("Moves length: " + interpreter.Moves.Count);
+}
 
+void RunGCodes()
+{
+    while (interpreter.Moves.Count == 0 || Globals.brake || Globals.stop) continue;
+    interpreter.Moves[0].Print();
+    axisControl.Move(interpreter.Moves[0]);
+    interpreter.Moves.RemoveAt(0);
 }
 
 
