e3d09a8a877258e694c9ee2726b1f873e72ac1af
KameelDeVliegtuig <126000217+KameelDeVliegtuig@users.noreply.github.com>
2024-06-21 15:00:57 +0200

    Removed debug lines

--- Code/CNC_Interpreter_V2/AxisControl.cs
+++ Code/CNC_Interpreter_V2/AxisControl.cs
@@ -129,7 +129,6 @@ namespace CNC_Interpreter_V2
 
                                 if (stepsDone[i] >= stepsToDo[i])
                                 {
-                                    Console.WriteLine("Axis " + (StepperAxis)i + " done");
                                     done[i] = true;
                                 }
                             }
