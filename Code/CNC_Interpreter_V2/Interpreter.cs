9424ef2637195d25d21bc18a99776f7f6dd54cfe
KameelDeVliegtuig <kamiel.groot@gmail.com>
2024-07-02 13:32:53 +0200

    en nu?

--- Code/CNC_Interpreter_V2/Interpreter.cs
+++ Code/CNC_Interpreter_V2/Interpreter.cs
@@ -277,7 +277,7 @@ namespace CNC_Interpreter_V2
                     try
                     {
                         Console.WriteLine("Given filename: " + dir + value.OpenText);
-                        Console.WriteLine("Existing file: " + File.Exists(dir+value.OpenText));
+                        Console.WriteLine("Existing file: " + File.Exists("@" +dir+value.OpenText));
                         if (value.OpenText != null && File.Exists(dir + value.OpenText))
                         {
                             fileManager.SetFile = dir + value.OpenText;
